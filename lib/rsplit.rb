a3a3045cc45885e22e3af33096fddbc464dd8ad8
OTSUKA Tatsuya <tatzyr@gmail.com>
2015-07-14 12:46:32 +0900

    Finish implementation

--- lib/rsplit.rb
+++ lib/rsplit.rb
@@ -1,5 +1,39 @@
 require "rsplit/version"
 
 module RSplit
-  # Your code goes here...
+  def rsplit(str, sep = nil, limit = 0)
+    valid_encoding?(str)
+    sep = $; if sep.nil?
+    case sep
+    when NilClass
+      # do nothing
+    when String
+      valid_encoding?(sep)
+      sep = sep.reverse
+    else
+      raise TypeError, "wrong argument type #{sep.class} (expected String)"
+    end
+
+    str.reverse.split(sep, limit).map(&:reverse).reverse
+  end
+
+  module_function :rsplit
+
+  # Avoid Bug #11387
+  # https://bugs.ruby-lang.org/issues/11387
+  def self.valid_encoding?(string)
+    raise ArgumentError, "invalid byte sequence in #{string.encoding.name}" unless string.valid_encoding?
+  end
+end
+
+
+class String
+  # :call-seq:
+  #   rsplit(sep = $;, limit = 0)
+  #
+  # Divides string into substrings based on a delimiter (starting from right), returning an array of these substrings.
+  #
+  def rsplit(*args)
+    RSplit.rsplit(self, *args)
+  end
 end
